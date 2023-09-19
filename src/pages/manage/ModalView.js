7c09c7c1c33884d5d2bc62453bbeea86a813f0e0
Olivier Abdelmoula <111987637+ora77@users.noreply.github.com>
2023-09-19 10:15:01 +0200

    fake-form-data

--- src/pages/manage/ModalView.js
+++ src/pages/manage/ModalView.js
@@ -1,13 +1,38 @@
-function SucessModal() {
-    const modalStyle = {border:"3px solid red", display:"flex",flexDirection:"column",alignItems:"center",height:"15rem",width:"50%",margin:"auto",gap:"1rem"}
+function SucessModal({ appointmentFormData }) {
+  const modalStyle = {
+    border: "3px solid red",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    height: "15rem",
+    width: "50%",
+    margin: "auto",
+    gap: "1rem",
+  };
   return (
-    <div className="modal-wrapper" style={{border:"3px solid red", display:"flex",flexDirection:"column",alignItems:"center",height:"15rem",width:"50%",margin:"auto",gap:"1rem"}}>
-      <h1 style={{color: "green"}}>Sucess!</h1>
-      <button style={{borderRadius: "15px",padding:"0.5rem",width:"25%"}}>
-        <a href="/" download="test.ics"> Download ics</a>
-       
-        </button>
-      <button style={{borderRadius: "15px",padding:"0.5rem",width:"25%"}}>Send recap email</button>
+    <div
+      className="modal-wrapper"
+      style={{
+        border: "3px solid red",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "15rem",
+        width: "50%",
+        margin: "auto",
+        gap: "1rem",
+      }}
+    >
+      <h1 style={{ color: "green" }}>Sucess!</h1>
+      <button style={{ borderRadius: "15px", padding: "0.5rem", width: "25%" }}>
+        <a href="/" download="test.ics">
+          {" "}
+          Download ics
+        </a>
+      </button>
+      <button style={{ borderRadius: "15px", padding: "0.5rem", width: "25%" }}>
+        Send recap email
+      </button>
     </div>
   );
 }
--- src/pages/manage/index.js
+++ src/pages/manage/index.js
@@ -11,10 +11,17 @@ export default function Manage() {
 
   const waitingRooms = new Map(Object.entries(data));
 
+  const appointmentFormData = new FormData();
+  appointmentFormData.append("nom", "abdelmoula");
+  appointmentFormData.append("prenom", "olivier");
+  appointmentFormData.append("noSS", "1910175104446");
+  appointmentFormData.append("tel", "0102030405");
+  appointmentFormData.append("mail", "olivier.abdelmoula@gmail.com");
+
   return (
     <div>
     <ManageView waitingRooms={waitingRooms}/>
-    <SucessModal/>
+    <SucessModal appointmentFormData={appointmentFormData}/>
 
     </div>
   );
